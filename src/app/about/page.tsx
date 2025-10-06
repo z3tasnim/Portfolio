f3610e68aa664e7cb9d8b9f67463007e7a25bdb2
Zarin <ztasnim263@gmail.com>
2025-10-06 19:16:23 +0600

    About Page done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaCode } from 'react-icons/fa'
+import { FaCode, FaGraduationCap, FaLaptopCode } from 'react-icons/fa'
 
 const About = () => {
   return (
@@ -15,6 +15,63 @@ const About = () => {
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
           <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md'>
             <FaCode className='h-8 w-8 text-primary mb-4' />
+            <h3 className='text-xl font-semibold mb-2'>Frontend</h3>
+            <ul className='text-secondary space-y-2'>
+              <li>React / Next.js</li>
+              <li>Typescript</li>
+              <li>Tailwind CSS</li>
+              <li>HTML5 / CSS3</li>
+            </ul>
+          </div>
+
+          <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md'>
+            <FaLaptopCode className='h-8 w-8 text-primary mb-4' />
+            <h3 className='text-xl font-semibold mb-2'>Backend</h3>
+            <ul className='text-secondary space-y-2'>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>MySQL</li>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+
+          <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md'>
+            <FaGraduationCap className='h-8 w-8 text-primary mb-4' />
+            <h3 className='text-xl font-semibold mb-2'>Tools & Ohters</h3>
+            <ul className='text-secondary space-y-2'>
+              <li>Git / GitHub</li>
+              <li>Docker</li>
+              <li>AWS</li>
+              <li>CLI / CD</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      {/* <section className='mb-16'>
+        <h2 className='section-title'>Experience</h2>
+
+        <div className='max-w-3xl mx-auto space-y-8'>
+          <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md'>
+          <h3 className='text-xl font-semibold mb-2'>Senior Frontend Developer</h3>
+          <p className='text-primary mb-2'>Company Name & Duration</p>
+          <ul className='text-secondary space-y-2 list-disc list-inside'>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, harum.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, harum.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, harum.</li>
+          </ul>
+          </div>
+        </div>
+      </section> */}
+
+      <section className='mb-16'>
+        <h2 className='section-title'>Education</h2>
+
+        <div className='max-w-3xl mx-auto space-y-8'>
+          <div className='bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md'>
+          <h3 className='text-xl font-semibold mb-2'>Bachelor of Science in Computer Science & Engineering</h3>
+          <p className='text-primary mb-2'>Sylhet Engineering College</p>
+          <p className='text-secondary'>2022 - Present</p>
           </div>
         </div>
       </section>
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className='container max-w-7xl mx-auto px-4 py-8'>
             <div className='flex flex-col md:flex-row justify-between items-center'>
                 <div className='mb-4 md:mb-0'>
-                    <p className='text-sm'>© {new Date().getFullYear()} Zarin Tasnim Chowdhury. All rights reserved.</p>
+                    <p className='text-sm text-gray-600 dark:text-gray-300'>© {new Date().getFullYear()} Zarin Tasnim Chowdhury. All rights reserved.</p>
                 </div>
                 <div className='flex space-x-6'>
               <Link href="/" className='text-2xl text-gray-600 hover:text-primary dark:text-gray-300 transition-colors duration-300'>
